61d182ad3b88ad63cebc1c39d278b565591c507f
Andrew Brown <drusepth@gmail.com>
2013-12-30 18:55:26 +0000

    continue scraping reddit while thrunder comment throttle

--- tentacles/reddit.py
+++ tentacles/reddit.py
@@ -13,7 +13,7 @@ from tentacle_base import Tentacle
 class Reddit_Tentacle(Tentacle):
   def __init__(self, config):
     self.response_rate = 0.001 # %
-    self.response_length = 6   # has some wiggle room
+    self.response_length = 12   # has some wiggle room
     self.dictionary_req = 2000 # num words in dictionary before trying to respond
 
     self.useragent = 'the internet beast by /u/drusepth'
@@ -44,10 +44,12 @@ class Reddit_Tentacle(Tentacle):
       subreddit = r.get_subreddit('all')
 
       for comment in subreddit.get_comments():
+        responded = False
         if random.randint(0, self.response_rate * 10000) / 100 == 0 and \
            comment.id not in replied_to and \
            str(comment.author) != self.username and \
-           markov.word_size() > self.dictionary_req:
+           markov.word_size() > self.dictionary_req and \
+           not responded:
 
           try:
             response = markov.generate_markov_text(self.response_length + random.randint(-5, 5))
@@ -58,10 +60,11 @@ class Reddit_Tentacle(Tentacle):
 
             if len(response) > 0:
               Tentacle.report(self, 'Responding')
+              responded = True
               comment.reply(response)
               replied_to.append(comment.id)
               comment.upvote()
-              time.sleep(300 + random.randint(150, 300))
+              time.sleep(30 + random.randint(30, 60))
             else:
               Tentacle.report(self, 'Not enough in cache to respond yet')
 
